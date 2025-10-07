875cbba8ea358e6b80e29d695edb071b2f58e2cb
Koza <skylercomstock69@gmail.com>
2025-10-07 15:03:53 -0400

    Update fxmanifest.lua

--- flightlicense/fxmanifest.lua
+++ flightlicense/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Koza'
-description 'Flight License System for VMS Flgiht School'
+description 'Flight License System for VMS Flight School'
 version '1.0.0'
 
 client_scripts {
@@ -13,3 +13,4 @@ dependencies {
     'qb-core'
 
 }
+
